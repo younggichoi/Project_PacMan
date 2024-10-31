fe7dca657319132f65f3d3c68b60452e99da6cf2
2-hyunwoo <hwoo1029@snu.ac.kr>
2024-10-31 20:10:35 +0900

    practice

--- PacMan/Shape3D.h
+++ PacMan/Shape3D.h
@@ -5,6 +5,7 @@
 //  Created by 이현우 on 10/31/24.
 //
 
+/*
 Shape3D();
 Shape3D(float x, float y, float z);
 void setCenter(float x, float y, float z);
@@ -42,6 +43,7 @@ class Shape3D{
 public:
     //void setShape(SHAPE shp);
     Shape3D();
+    /*
     void setPos(float x, float y, float z);
     void setAngleOfSpin(float ang);
     void setAngleOfRevolution(float ang);
@@ -50,6 +52,22 @@ public:
     void increaseAngleOfSpin(float ang);
     void increaseAngleOfvolution(float ang);
     void draw() const;
+     */
+    Shape3D(float x, float y, float z);
+    void setCenter(float x, float y, float z);
+    void setCenter(const Vector3f& c);
+    Vector3f getCenter() const;
+    void setVelocity(float x, float y, float z);
+    void setVelocity(const Vector3f& v);
+    Vector3f getVelocity() const;
+    void setMTL(const Material& m);
+    const Material& getMTL() const;
+    void move();
+    virtual void draw() const = 0;
+protected:
+    Vector3f center;
+    Vector3f velocity;
+    Material mtl;
     
     
     //SHAPE shape;
