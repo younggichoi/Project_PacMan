0d6d3c520b098be655cdbb7ff8111d9aa07e0005
2-hyunwoo <hwoo10292snu.ac.kr>
2024-11-15 17:01:55 +0900

    edited collisionhandler

--- PacMan/CollisionHandler.cpp
+++ PacMan/CollisionHandler.cpp
@@ -15,17 +15,31 @@ void CollisionHandler::operator()(PacMan& pacman, const Map& map){
     }
 }
 void CollisionHandler::operator()(PacMan& pacman, Ghost& ghost){
-    //TODO
-    if ()
+    // 만난 ghost가 frightened 상태라면 ghost->eaten, ghostroom으로 
+    // else: life 줄이고 pacman, ghost들 처음 위치로.
+    if (ghost.getState() == Ghost::FRIGHTENED) {
+        ghost.setState(Ghost::EATEN);
+    }
+    else {
+        pacman.decreaseLife();
+        // pacman, ghost들 각각 처음 위치로.
+        // TODO
+        
+    }
 }
 void CollisionHandler::operator()(PacMan& pacman, Dot& dot) {
     //TODO
+    // dot 지우기
+    dot.setEaten(true);
     if (dot.isLarge()) {
-        // 큰 dot 먹은 경우 ghost들 상태 바꾸기
-
+        // 큰 dot 먹은 경우 ghost들 상태 frightened로 바꾸기 (eaten이 아니라면.)
+        // ghost들 전역변수로 선언하고 constants.h에 넣은 후에 아래 주석표시 풀기
+        /*
+        if (ghost.getState() != Ghost::EATEN) {
+            ghost.setState(Ghost::FRIGHTENED);
+        }
+        */
     }
-    else {
-        // 작은 dot 먹은 경우 점수올리기?
-
-    }
+    // 점수올리기
+    pacman.addScore(50);
 }
