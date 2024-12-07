0f1f27eb5043966ea2ce8ec280622b66d7956dff
2-hyunwoo <81797496+2-hyunwoo@users.noreply.github.com>
2024-12-07 21:51:47 +0900

    cheatkey

--- PacMan/specialkey.cpp
+++ PacMan/specialkey.cpp
@@ -1,8 +1,13 @@
 #include <GL/freeglut.h>
 #include "specialkey.h"
 #include "Sphere.h"
+#include "constant.h"
 
 extern PacMan pacman;
+extern WINDOW_STATE windowState;
+extern bool currStageClear, allStageClear, STOPFLAG;
+extern int stage_num, eTime;
+extern float stageClear_sTime;
 
 // keys : GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_KEY_DOWN
 
@@ -26,6 +31,16 @@ void specialkey_ingame(int key, int x, int y)
 		break;
 	case GLUT_KEY_DOWN:
 		pacman.setNextDirection(Sphere::DIRECTION::DOWN);
+		break;
+	case GLUT_KEY_F1:
+		if (stage_num < 2) {
+			currStageClear = true;
+		}
+		else if (stage_num == 2) {
+			allStageClear = true;
+		}
+		STOPFLAG = true;
+		stageClear_sTime = eTime;
 		break;
 	default:
 		break;
